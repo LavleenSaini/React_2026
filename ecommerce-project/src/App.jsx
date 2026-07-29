4da1536f8c40d56a163453535bca5a32e1f992cc
Lavleen Saini <lavleen1128@gmail.com>
2026-07-29 18:45:29 -0400

    Set up html of tracking and orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,13 +2,17 @@ import { HomePage } from './pages/HomePage'
 import { Routes } from 'react-router'
 import { Route } from 'react-router'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
 import './App.css'
+import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element ={<CheckoutPage />} />
+      <Route path="orders" element ={<OrdersPage />} />
+      <Route path="tracking" element ={<TrackingPage />} />
     </Routes>
     
   )
