df268ec87417baecd54c5c29c5da1c1c7cf27f38
Lavleen Saini <lavleen1128@gmail.com>
2026-07-28 23:27:20 -0400

    Created multiple pages using react-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,14 @@
 import { HomePage } from './pages/HomePage'
+import { Routes } from 'react-router'
+import { Route } from 'react-router'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+    
   )
 }
 
